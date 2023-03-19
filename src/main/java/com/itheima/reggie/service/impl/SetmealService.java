4db23b592b332019c9e80b855f5af034153359ca
Luka <luka_jin@outlook.com>
2023-03-20 10:07:30 +1100

    third version

--- src/main/java/com/itheima/reggie/service/impl/SetmealService.java
+++ src/main/java/com/itheima/reggie/service/impl/SetmealService.java
@@ -1,8 +1,13 @@
 package com.itheima.reggie.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.itheima.reggie.dto.SetmealDto;
 import com.itheima.reggie.entity.Setmeal;
+import org.springframework.transaction.annotation.Transactional;
 
 
 public interface SetmealService extends IService<Setmeal> {
+    @Transactional
+    public void saveWithDish(SetmealDto setmealDto);
+
 }
--- src/main/java/com/itheima/reggie/service/impl/impl/SetmealServiceImpl.java
+++ src/main/java/com/itheima/reggie/service/impl/impl/SetmealServiceImpl.java
@@ -1,13 +1,42 @@
 package com.itheima.reggie.service.impl.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.itheima.reggie.dto.SetmealDto;
 import com.itheima.reggie.entity.Setmeal;
+import com.itheima.reggie.entity.SetmealDish;
 import com.itheima.reggie.mapper.SetmealMapper;
+import com.itheima.reggie.service.impl.SetmealDishService;
 import com.itheima.reggie.service.impl.SetmealService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
 public class SetmealServiceImpl extends ServiceImpl<SetmealMapper, Setmeal> implements SetmealService {
+    @Autowired
+    private SetmealDishService setmealDishService;
+    /**
+     * 新增套餐，同时新增套餐和菜品的关系
+     * @param setmealDto
+     */
+    @Transactional
+    public void saveWithDish(SetmealDto setmealDto) {
+        //保存套餐的基本信息
+        this.save(setmealDto);
+    
+        //保存套餐和菜品的关系
+        List<SetmealDish> setmealDishes = setmealDto.getSetmealDishes();
+        setmealDishes.stream().map((item)->{
+            item.setSetmealId(setmealDto.getId());
+            return item;
+        }).collect(Collectors.toList());
+
+        setmealDishService.saveBatch(setmealDishes);
+
+    }
 }
